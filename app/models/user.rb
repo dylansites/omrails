5bff79fd0f28bb9834f93771ac332937152c4aa8
Dylan Sites <dylan.sites@nyu.edu>
2013-06-14 20:18:46 -0400

    temporarily remove recoverable module

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
+          :rememberable, :trackable, :validatable
+    # add the recoverable thing below to the devise section above to include
+    # forgot your password functionability
+	#:recoverable, 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
